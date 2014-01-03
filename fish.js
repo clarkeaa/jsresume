7e1f49872e58ffa6b07ed4ce4205422e215d0f10
Aaron Clarke <clarkeaa@gmail.com>
2014-01-03 11:34:23 -0800

    started drawing the loc widget

--- fish.js
+++ fish.js
@@ -1,10 +1,21 @@
 var CTX = null;
 var WIDTH = 640;
 var HEIGHT = 480;
+var POSITION = 0;
+var LOCWIDGETHEIGHT = 20;
+function drawLocWidget() {
+    CTX.fillStyle = '#ff0000';
+    CTX.lineWidth = 1;
+    CTX.strokeStyle = '#00ff00';
+    var locWidgetTop = HEIGHT - LOCWIDGETHEIGHT;
+    CTX.fillRect(0, locWidgetTop, WIDTH, LOCWIDGETHEIGHT);
+    return CTX.strokeRect(0, locWidgetTop, WIDTH, LOCWIDGETHEIGHT);
+};
 function draw() {
-    CTX.clearRect(0, 0, 640, 480);
+    CTX.clearRect(0, 0, WIDTH, HEIGHT);
     CTX.fillStyle = '#0000ff';
-    return CTX.fillRect(0, 0, 640, 480);
+    CTX.fillRect(0, 0, WIDTH, HEIGHT);
+    return drawLocWidget();
 };
 function main() {
     var elem = document.getElementById('resume-canvas');
--- fish.lisp
+++ fish.lisp
@@ -2,11 +2,26 @@
 (defvar *ctx* nil)
 (defvar *width* 640)
 (defvar *height* 480)
+(defvar *position* 0)
+(defvar *loc-widget-height* 20)
+
+(defmacro ctx (func &rest args) (cons `(@ *ctx* ,func) args))
+(defmacro clear () `(ctx clearRect 0 0 *width* *height*))
+
+(defun draw-loc-widget ()
+  (setf (@ *ctx* fillStyle) "#ff0000")
+  (setf (@ *ctx* lineWidth) 1)
+  (setf (@ *ctx* strokeStyle) "#00ff00")
+  (let ((loc-widget-top (- *height* *loc-widget-height*)))
+    (ctx fillRect 0 loc-widget-top *width* *loc-widget-height*)
+    (ctx strokeRect 0 loc-widget-top *width* *loc-widget-height*)))
 
 (defun draw ()
-  ((@ *ctx* clearRect) 0 0 640 480)
+  (clear)
   (setf (@ *ctx* fillStyle) "#0000ff")
-  ((@ *ctx* fillRect) 0 0 640 480))
+  (ctx fillRect 0 0 *width* *height*)
+  (draw-loc-widget)
+  )
 
 (defun main ()
   (let ((elem ((@ document getElementById) "resume-canvas")))
