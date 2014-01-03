f99f6709eb53ec0aa92c42195b8fbc7ea5f9eebc
Aaron Clarke <clarkeaa@gmail.com>
2014-01-03 14:18:08 -0800

    got the fish drawing

--- fish.js
+++ fish.js
@@ -5,6 +5,8 @@ var POSITION = 0;
 var LOCWIDGETHEIGHT = 20;
 var LOCWIDGETKNOBWIDTH = 20;
 var GROUNDHEIGHT = 100;
+var FPS = 30;
+var FRAME = 0;
 function drawLocWidget() {
     CTX.fillStyle = '#ff0000';
     CTX.lineWidth = 1;
@@ -33,7 +35,7 @@ function drawGround() {
     for (var y = 0; y < GROUNDHEIGHT; y += 1) {
         for (var x = 0; x < WIDTH; x += 1) {
             var index = 4 * (x + WIDTH * y);
-            var depth = 1.0 + 0.5 * Math.sin(x * 1 + 7 * y + POSITION);
+            var depth = 1.0 + 0.5 * Math.sin(x * 1 + 7 * y * y + POSITION);
             pix[0 + index] = depth * 200;
             pix[1 + index] = depth * 100;
             pix[2 + index] = 0;
@@ -41,11 +43,28 @@ function drawGround() {
     };
     return CTX.putImageData(imgData, 0, groundTop);
 };
+function drawAaronFish() {
+    var fishIndex = Math.floor(FRAME / 10) % 3;
+    var fish = null;
+    switch (fishIndex) {
+    case 0:
+        fish = document.getElementById('fish1');
+        break;
+    case 1:
+        fish = document.getElementById('fish2');
+        break;
+    case 2:
+        fish = document.getElementById('fish3');
+    };
+    return CTX.drawImage(fish, 0, 0);
+};
 function draw() {
     CTX.clearRect(0, 0, WIDTH, HEIGHT);
+    FRAME += 1;
     CTX.fillStyle = '#0000ff';
     CTX.fillRect(0, 0, WIDTH, HEIGHT);
     drawGround();
+    drawAaronFish();
     return drawLocWidget();
 };
 function main() {
@@ -56,7 +75,7 @@ function main() {
             CTX = ctx;
             draw();
             document.onkeydown = handleKeyboard;
-            return setInterval(draw, 30);
+            return setInterval(draw, FPS);
         };
     };
 };
